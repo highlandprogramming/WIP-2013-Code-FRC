d3abb1761deba275443eaba6bd2c8bd6ae8fcb4c
HRT3 Laptop Highand Robotics 2603 <normpearii@gmail.com>
2013-01-23 17:18:43 -0500

    Vision/Pneumatics
    Added Vision Code
    Added Pneumatics
    Added new SmartDashboard components
    Added camera to smartDashboard.

--- 2603NML/MyRobot.cpp
+++ 2603NML/MyRobot.cpp
@@ -5,6 +5,9 @@
 #include <cmath>
 #include "DigitalInput.h"
 #include "SmartDashboard/SmartDashboard.h"
+#include "Vision/RGBImage.h"
+#include "Vision/BinaryImage.h"
+#include "Math.h"
 //#include "SmartDashboard/SendableChooser.h"
 //#include "SmartDashboard/Sendable.h"
 //#include "SmartDashboard/NamedSendable.h"
@@ -12,8 +15,47 @@
 //#include "BinaryImage.h"
 //#include "VisionAPI.h"
 
+//Camera constants used for distance calculation
+#define X_IMAGE_RES 320		//X Image resolution in pixels, should be 160, 320 or 640
+#define VIEW_ANGLE 48		//Axis 206 camera
+//#define VIEW_ANGLE 43.5  //Axis M1011 camera
+#define PI 3.141592653
+
+//Score limits used for target identification
+#define RECTANGULARITY_LIMIT 60
+#define ASPECT_RATIO_LIMIT 75
+#define X_EDGE_LIMIT 40
+#define Y_EDGE_LIMIT 60
+
+//Minimum area of particles to be considered
+#define AREA_MINIMUM 500
+
+//Edge profile constants used for hollowness score calculation
+#define XMAXSIZE 24
+#define XMINSIZE 24
+#define YMAXSIZE 24
+#define YMINSIZE 48
+const double xMax[XMAXSIZE] = {1, 1, 1, 1, .5, .5, .5, .5, .5, .5, .5, .5, .5, .5, .5, .5, .5, .5, .5, .5, 1, 1, 1, 1};
+const double xMin[XMINSIZE] = {.4, .6, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, 0.6, 0};
+const double yMax[YMAXSIZE] = {1, 1, 1, 1, .5, .5, .5, .5, .5, .5, .5, .5, .5, .5, .5, .5, .5, .5, .5, .5, 1, 1, 1, 1};
+const double yMin[YMINSIZE] = {.4, .6, .05, .05, .05, .05, .05, .05, .05, .05, .05, .05, .05, .05, .05, .05, .05, .05, .05, .05, .05, .05,
+								.05, .05, .05, .05, .05, .05, .05, .05, .05, .05, .05, .05, .05, .05, .05, .05, .05, .05, .05, .05, .05, .05,
+								.05, .05, .6, 0};
+
+
 class RobotDemo : public SimpleRobot
 {
+	
+	struct Scores
+	{
+		double rectangularity;
+		double aspectRatioInner;
+		double aspectRatioOuter;
+		double xEdge;
+		double yEdge;
+
+	};
+	
 	//Declare-----------
 	//SmartDashboard *sd;
 	RobotDrive myRobot;
@@ -24,6 +66,9 @@ class RobotDemo : public SimpleRobot
 	Compressor *compressor;
 	Solenoid *s[8];
 	PIDOutput *pidOutput;
+	Scores *scores;
+//	char particle[];
+	
 	
 public:
 	RobotDemo(void):
@@ -74,7 +119,7 @@ public:
 	void Autonomous(void)
 	{
 		compressor->Start();
-		GetWatchdog().SetEnabled(true);
+		GetWatchdog().SetEnabled(false);
 		GetWatchdog().SetExpiration(10);
 		GetWatchdog().Feed();
 		myRobot.SetSafetyEnabled(false);
@@ -82,6 +127,102 @@ public:
 		Wait(2.0); 				
 		myRobot.Drive(0.0, 0.0); 	
 		*/
+		
+		int x = 0;
+		
+		
+		
+		Threshold threshold(60, 100, 90, 255, 20, 255);	//HSV threshold criteria, ranges are in that order ie. Hue is 60-100
+		ParticleFilterCriteria2 criteria[] = {
+				{IMAQ_MT_AREA, AREA_MINIMUM, 65535, false, false}
+		};												//Particle filter criteria, used to filter out small particles
+		// AxisCamera &camera = AxisCamera::GetInstance();	//To use the Axis camera uncomment this line
+				
+		while (IsAutonomous() && IsEnabled())
+		{
+		          /**
+		             * Do the image capture with the camera and apply the algorithm described above. This
+		             * sample will either get images from the camera or from an image file stored in the top
+		             * level directory in the flash memory on the cRIO. The file name in this case is "testImage.jpg"
+		             */
+			if(x == 0)
+			{
+			AxisCamera &camera = AxisCamera::GetInstance("10.26.3.11");
+			camera.WriteResolution(AxisCamera::kResolution_320x240);
+			camera.WriteCompression(20);
+			camera.WriteBrightness(50);
+			
+			ColorImage *image;
+			//image = new RGBImage("/testImage.jpg");		// get the sample image from the cRIO flash
+			image = camera.GetImage();
+					//camera.GetImage(image);				//To get the images from the camera comment the line above and uncomment this one
+			BinaryImage *thresholdImage = image->ThresholdHSV(threshold);	// get just the green target pixels
+					//thresholdImage->Write("/threshold.bmp");
+			BinaryImage *convexHullImage = thresholdImage->ConvexHull(false);  // fill in partial and full rectangles
+					//convexHullImage->Write("/ConvexHull.bmp");
+			BinaryImage *filteredImage = convexHullImage->ParticleFilter(criteria, 1);	//Remove small particles
+					//filteredImage->Write("Filtered.bmp");
+
+			vector<ParticleAnalysisReport> *reports = filteredImage->GetOrderedParticleAnalysisReports();  //get a particle analysis report for each particle
+			scores = new Scores[reports->size()];
+					
+					//Iterate through each particle, scoring it and determining whether it is a target or not
+			for (unsigned i = 0; i < reports->size(); i++)
+			{
+				ParticleAnalysisReport *report = &(reports->at(i));
+						
+				scores[i].rectangularity = scoreRectangularity(report);
+				scores[i].aspectRatioOuter = scoreAspectRatio(filteredImage, report, true);
+				scores[i].aspectRatioInner = scoreAspectRatio(filteredImage, report, false);			
+				scores[i].xEdge = scoreXEdge(thresholdImage, report);
+				scores[i].yEdge = scoreYEdge(thresholdImage, report);
+						
+				if(scoreCompare(scores[i], false))
+				{
+					
+					printf("particle: %d  is a High Goal  centerX: %f  centerY: %f \n", i, report->center_mass_x_normalized, report->center_mass_y_normalized);
+					//string particle = ("particle: %d  is a High Goal  centerX: %f  centerY: %f \n", i, report->center_mass_x_normalized, report->center_mass_y_normalized);
+					SmartDashboard::PutNumber("CenterX", report->center_mass_x);
+					SmartDashboard::PutNumber("CenterY", report->center_mass_y);
+					printf("Distance: %f \n", computeDistance(thresholdImage, report, false));
+					SmartDashboard::PutNumber("Distance",computeDistance(thresholdImage,report, false));
+				} 
+				
+				else if (scoreCompare(scores[i], true))
+				{
+					printf("particle: %d  is a Middle Goal  centerX: %f  centerY: %f \n", i, report->center_mass_x_normalized, report->center_mass_y_normalized);
+					printf("Distance: %f \n", computeDistance(thresholdImage, report, true));
+					SmartDashboard::PutNumber("CenterX", report->center_mass_x);
+					SmartDashboard::PutNumber("CenterY", report->center_mass_y);
+					SmartDashboard::PutNumber("Distance",computeDistance(thresholdImage,report, false));
+				
+				}
+				
+				else
+				{
+					printf("particle: %d  is not a goal  centerX: %f  centerY: %f \n", i, report->center_mass_x_normalized, report->center_mass_y_normalized);
+					SmartDashboard::PutNumber("CenterX", report->center_mass_x);
+					SmartDashboard::PutNumber("CenterY", report->center_mass_y);
+										
+				}
+				printf("rect: %f  ARinner: %f \n", scores[i].rectangularity, scores[i].aspectRatioInner);
+				printf("ARouter: %f  xEdge: %f  yEdge: %f  \n", scores[i].aspectRatioOuter, scores[i].xEdge, scores[i].yEdge);	
+			}
+			printf("\n");
+					
+					// be sure to delete images after using them
+			delete filteredImage;
+			delete convexHullImage;
+			delete thresholdImage;
+			delete image;
+					
+					//delete allocated reports and Scores objects also
+			delete scores;
+			delete reports;
+				}
+			x++;
+		}
+		
 	}
 
 	void OperatorControl(void)
@@ -89,20 +230,16 @@ public:
 		compressor->Enabled();
 		compressor->Start();
 		myRobot.SetSafetyEnabled(true);
-		GetWatchdog().SetEnabled(true);
+		GetWatchdog().SetEnabled(false);
 		GetWatchdog().SetExpiration(1);
 		GetWatchdog().Feed();
-		AxisCamera &camera = AxisCamera::GetInstance("10.26.3.11");
-		camera.WriteResolution(AxisCamera::kResolution_320x240);
-		camera.WriteCompression(20);
-		camera.WriteBrightness(50);
 		
 		GetWatchdog().Feed();
 		
 		//sd->sendIOPortData();
 
 		// Local variables.
-		bool blnFire, blnpiston;
+		bool blnFire, blnPiston;
 		
 		while (IsOperatorControl())
 		{
@@ -112,14 +249,17 @@ public:
 			SmartDashboard::PutNumber("Demo",3);
 			
 			GetWatchdog().Feed();
-			if(stick1->GetTrigger() && blnpiston == false)
+			if(stick1->GetTrigger())
 			{
+				GetWatchdog().Feed();
 				s[0]->Set(true);
+				SmartDashboard::PutBoolean("Piston Out",true);
 				Wait(.5);
 				GetWatchdog().Feed();
 				Wait(.5);
 				GetWatchdog().Feed();
 				s[0]->Set(false);
+				SmartDashboard::PutBoolean("Piston Out",false);
 			}
 			
 			if(stick2->GetTrigger() && blnFire == false)
@@ -144,6 +284,88 @@ public:
 			GetWatchdog().Feed();
 		}
 	}
+	
+	double computeDistance (BinaryImage *image, ParticleAnalysisReport *report, bool outer) {
+		double rectShort, height;
+		int targetHeight;
+		
+		imaqMeasureParticle(image->GetImaqImage(), report->particleIndex, 0, IMAQ_MT_EQUIVALENT_RECT_SHORT_SIDE, &rectShort);
+		//using the smaller of the estimated rectangle short side and the bounding rectangle height results in better performance
+		//on skewed rectangles
+		height = min(report->boundingRect.height, rectShort);
+		targetHeight = outer ? 29 : 21;
+		
+		return X_IMAGE_RES * targetHeight / (height * 12 * 2 * tan(VIEW_ANGLE*PI/(180*2)));
+	}
+	
+	double scoreAspectRatio(BinaryImage *image, ParticleAnalysisReport *report, bool outer){
+		double rectLong, rectShort, idealAspectRatio, aspectRatio;
+		idealAspectRatio = outer ? (62/29) : (62/20);	//Dimensions of goal opening + 4 inches on all 4 sides for reflective tape
+		
+		imaqMeasureParticle(image->GetImaqImage(), report->particleIndex, 0, IMAQ_MT_EQUIVALENT_RECT_LONG_SIDE, &rectLong);
+		imaqMeasureParticle(image->GetImaqImage(), report->particleIndex, 0, IMAQ_MT_EQUIVALENT_RECT_SHORT_SIDE, &rectShort);
+		
+		//Divide width by height to measure aspect ratio
+		if(report->boundingRect.width > report->boundingRect.height){
+			//particle is wider than it is tall, divide long by short
+			aspectRatio = 100*(1-fabs((1-((rectLong/rectShort)/idealAspectRatio))));
+		} else {
+			//particle is taller than it is wide, divide short by long
+			aspectRatio = 100*(1-fabs((1-((rectShort/rectLong)/idealAspectRatio))));
+		}
+		return (max(0, min(aspectRatio, 100)));		//force to be in range 0-100
+	}
+	
+	bool scoreCompare(Scores scores, bool outer){
+		bool isTarget = true;
+
+		isTarget &= scores.rectangularity > RECTANGULARITY_LIMIT;
+		if(outer){
+			isTarget &= scores.aspectRatioOuter > ASPECT_RATIO_LIMIT;
+		} else {
+			isTarget &= scores.aspectRatioInner > ASPECT_RATIO_LIMIT;
+		}
+		isTarget &= scores.xEdge > X_EDGE_LIMIT;
+		isTarget &= scores.yEdge > Y_EDGE_LIMIT;
+
+		return isTarget;
+	}
+	
+	double scoreRectangularity(ParticleAnalysisReport *report){
+		if(report->boundingRect.width*report->boundingRect.height !=0){
+			return 100*report->particleArea/(report->boundingRect.width*report->boundingRect.height);
+		} else {
+			return 0;
+		}	
+	}
+	
+	double scoreXEdge(BinaryImage *image, ParticleAnalysisReport *report){
+		double total = 0;
+		LinearAverages *averages = imaqLinearAverages2(image->GetImaqImage(), IMAQ_COLUMN_AVERAGES, report->boundingRect);
+		for(int i=0; i < (averages->columnCount); i++){
+			if(xMin[i*(XMINSIZE-1)/averages->columnCount] < averages->columnAverages[i] 
+			   && averages->columnAverages[i] < xMax[i*(XMAXSIZE-1)/averages->columnCount]){
+				total++;
+			}
+		}
+		total = 100*total/(averages->columnCount);		//convert to score 0-100
+		imaqDispose(averages);							//let IMAQ dispose of the averages struct
+		return total;
+	}
+	
+	double scoreYEdge(BinaryImage *image, ParticleAnalysisReport *report){
+		double total = 0;
+		LinearAverages *averages = imaqLinearAverages2(image->GetImaqImage(), IMAQ_ROW_AVERAGES, report->boundingRect);
+		for(int i=0; i < (averages->rowCount); i++){
+			if(yMin[i*(YMINSIZE-1)/averages->rowCount] < averages->rowAverages[i] 
+			   && averages->rowAverages[i] < yMax[i*(YMAXSIZE-1)/averages->rowCount]){
+				total++;
+			}
+		}
+		total = 100*total/(averages->rowCount);		//convert to score 0-100
+		imaqDispose(averages);						//let IMAQ dispose of the averages struct
+		return total;
+	}	
 };
 
 START_ROBOT_CLASS(RobotDemo);
